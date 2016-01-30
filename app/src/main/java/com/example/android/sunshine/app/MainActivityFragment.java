4814b3416ef066a9e98956299f1eda58afa633f1
Chris Nwamba <Chris Nwamba>
2016-01-30 07:51:46 +0100

    Completed lesson 1

---
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -0,0 +1,55 @@
+package com.example.android.sunshine.app;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Wednesday - Cloudy - 35/60",
+                "Thursday - Cloudy - 35/60",
+                "Friday - Cloudy - 35/60",
+                "Saturday - Cloudy - 35/60",
+                "Sunday - Cloudy - 35/60",
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast
+        );
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
+    }
+}
